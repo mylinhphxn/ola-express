9a5e5cde21c7d1a7c5dc9cd9e62ce56d5a7ef644
mylinhphxn <>
2022-08-10 16:02:07 +0000

    this change added express

--- index.js
+++ index.js
@@ -1,19 +1,12 @@
-var http = require("http"); 
-http.createServer(function(req,res) {
-  var path = req.url.toLowerCase();
-  switch(path) {
-    case '/':
-      res.writeHead(200, {'Content-Type': 'text/plain'});
-      res.end('Home page');
-      break;
-    case '/about':
-      res.writeHead(200, {'Content-Type': 'text/plain'});
-      res.end('About page');
-      break;
-    default:
-      res.writeHead(404, {'Content-Type': 'text/plain'});
-      res.end('Not found');
-      break;
-    }
-}).listen(process.env.PORT || 3000);
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('ola from express')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
 
