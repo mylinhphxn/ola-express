14c374b1909562dbc998978d6063a64c750e37bb
mylinhphxn <103076867+mylinhphxn@users.noreply.github.com>
2022-08-10 16:12:41 +0000

    about page change

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('ola from express')
+app.get('/about', (req, res) => {
+  res.send('this is our about page!')
 })
 
 app.listen(port, () => {
